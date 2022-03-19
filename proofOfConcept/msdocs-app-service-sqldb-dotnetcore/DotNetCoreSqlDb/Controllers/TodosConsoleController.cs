99fbb4dcb5b486163bcd66cf7130b60805b18dcf
Vera Ts <veratsvang@gmail.com>
2022-03-20 00:36:41 +0200

    Http Server made by following a guide from Microsoft, altered to accomodate API functionality

---
+++ proofOfConcept/msdocs-app-service-sqldb-dotnetcore/DotNetCoreSqlDb/Controllers/TodosConsoleController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DotNetCoreSqlDb.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreSqlDb.Controllers
+{
+    [Route("ToDos")]
+    [ApiController]
+    public class TodosConsoleController : ControllerBase
+    {
+        private readonly Models.MyDatabaseContext _context;
+        public TodosConsoleController(Models.MyDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Todos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
+        {
+            return await _context.Todo.ToListAsync();
+        }
+    }
+}
