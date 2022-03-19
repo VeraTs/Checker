eb15b803b514586ab74f6f3e5da721c31f3369aa
Vera Ts <veratsvang@gmail.com>
2022-03-20 00:13:33 +0200

    simple Http client for ToDoList using System.Net.Http.HttpClient

---
+++ proofOfConcept/HttpClientSampler/HttpClientSampler/Program.cs
@@ -0,0 +1,39 @@
+﻿using HttpClientSampler.TDOs;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HttpClientSampler
+{
+    public class Program
+    {
+        public static void Main()
+        {
+/*            ToDoList toDos = new ToDoList();
+            toDos.AddtoDo(new ToDo() { id = 0, createdDate = "1534T", description = "Tester1" });
+            toDos.AddtoDo(new ToDo() { id = 1, createdDate = "1534T", description = "Tester2" });
+            toDos.AddtoDo(new ToDo() { id = 2, createdDate = "1534T", description = "Tester3" });
+            Console.WriteLine(toDos);
+            String jsonStr = JsonSerializer.Serialize(toDos);
+            Console.WriteLine(jsonStr);
+
+*/
+
+            // using System.Text.Json
+            // create client and send request
+            ToDoHttpClient client = new ToDoHttpClient("http://coresqltester.azurewebsites.net/Todos/");
+            String str = client.SendRequest();
+            
+            // print response json
+            Console.WriteLine(str);
+
+            // generate the response into a ToDoList object (list of the DTO with prettier printing)
+            ToDoList? list2 = JsonSerializer.Deserialize<ToDoList>(str);
+            
+            // print list
+            Console.WriteLine(list2);
+
+        }
+    }
+}
+
+
---
+++ proofOfConcept/HttpClientSampler/HttpClientSampler/ToDoHttpClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientSampler
+{
+    internal class ToDoHttpClient
+    {
+        private readonly HttpClient client;
+        public ToDoHttpClient(String url)
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+        }
+        public String SendRequest()
+        {
+            client.Timeout = TimeSpan.FromMinutes(5);
+            Task<HttpResponseMessage> response = client.GetAsync("");
+            response.Wait();
+            if(response != null)
+            {
+                var stream = response.Result.Content.ReadAsStream();
+                byte[] buffer = new byte[1024];
+                StringBuilder res = new StringBuilder();
+                int offset = 0;
+                bool read = true;
+                int bytesRead = 0;
+                while(read)
+                {
+
+                    bytesRead = stream.Read(buffer, offset, buffer.Length);
+                    if(bytesRead > 0)
+                    {
+                        String temp = System.Text.Encoding.UTF8.GetString(buffer);
+                        res.Append(temp.Substring(0, bytesRead));
+                        offset += bytesRead;
+                    } else
+                    {
+                        read = false;
+                    }
+
+                    if(offset >= stream.Length)
+                    {
+                        read = false;
+                    }
+                }
+
+                return res.ToString();
+            }
+
+            return "";            
+        }
+    }
+}
