67a3529063df774d35b55289a1c2d8373cbaf2bc
Vera Ts <veratsvang@gmail.com>
2022-03-21 18:53:46 +0200

    Updated server to include JSON GET and POST

--- proofOfConcept/msdocs-app-service-sqldb-dotnetcore/DotNetCoreSqlDb/Controllers/TodosConsoleController.cs
+++ proofOfConcept/msdocs-app-service-sqldb-dotnetcore/DotNetCoreSqlDb/Controllers/TodosConsoleController.cs
@@ -3,10 +3,12 @@ using DotNetCoreSqlDb.Models;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System;
 
 namespace DotNetCoreSqlDb.Controllers
 {
-    [Route("ToDos")]
+    [Route("JsonToDos")]
     [ApiController]
     public class TodosConsoleController : ControllerBase
     {
@@ -16,11 +18,69 @@ namespace DotNetCoreSqlDb.Controllers
             _context = context;
         }
 
-        // GET: /Todos
+        // GET: /JsonToDos
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
         {
             return await _context.Todo.ToListAsync();
+        }
+
+        // GET: JsonTodos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Todo>> GetTodos(int id)
+        {
+            var res = await _context.Todo
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            if(res == null)
+            {
+                return NotFound();
+            }
+
+            return res;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> AddToDo(Todo todo)
+        {
+            
+            if (ModelState.IsValid && todo != null)
+            {
+                try
+                {
+                    _context.Todo.Add(todo);
+                    return await _context.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    String msg = "";
+                    if(todo.ID != 0)
+                    {
+                        msg = "You cannot decide an item id on your own";
+                    }
+                    else
+                    {
+                        msg = "a server error has occured";
+                    }
+                    return BadRequest(msg);
+                }
+                
+
+            }
+            else
+            {
+                String msg = "";
+                if(todo == null)
+                {
+                    msg += "Invalid Sytax for Object\n";
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    msg += "Internal error";
+                }
+                return BadRequest(msg);
+            }
         }
     }
 }
