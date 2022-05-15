33978e64b5764da685cedffaf5e5b69817a037a2
Aviv Barkai <avivbarkai3@gmail.com>
2022-05-15 23:37:56 +0300

    update 1
    A new app has been launched for the benefit of developing a waiter app
    At this point you should finish the page designed to create a new order

---
+++ WaitersApp/CheckerWaitersApp/CheckerWaitersApp/CheckerWaitersApp/ViewModels/DishViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CheckerWaitersApp.Models;
+
+namespace CheckerWaitersApp.ViewModels
+{
+    public class DishViewModel : BaseViewModel
+    {
+        private readonly DishModel m_model;
+
+        public DishViewModel(DishModel i_dish)
+        {
+            m_model = new DishModel();
+            m_model = i_dish;
+        }
+        public string DishName
+        {
+            get => m_model.m_DishName;
+            private set => m_model.m_DishName = value;
+        }
+        public string Description
+        {
+            get => m_model.m_Description;
+            set => m_model.m_Description = value;
+        }
+    }
+}
---
+++ WaitersApp/CheckerWaitersApp/CheckerWaitersApp/CheckerWaitersApp/Views/CreateOrderView/NewOrderCardView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CheckerWaitersApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CheckerWaitersApp.Views.CreateOrderView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewOrderCardView : ContentView
+    {
+        private DishesViewModel m_vm = new DishesViewModel();
+        public NewOrderCardView()
+        {
+            InitializeComponent();
+        }
+        public void SetViewModel(DishesViewModel i_ViewModel)
+        {
+            m_vm = i_ViewModel;
+            BindingContext = m_vm;
+        }
+    }
+}
