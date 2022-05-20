0dd454c243672ec41baa9926c2e0822c96ebb766
Vera Ts <veratsvang@gmail.com>
2022-05-20 23:37:33 +0300

    Altered order item enums to be more functional

--- CheckerServer/CheckerServer/Models/OrderItem.cs
+++ CheckerServer/CheckerServer/Models/OrderItem.cs
@@ -16,20 +16,20 @@ namespace CheckerServer.Models
 
 
         public string Changes { get; set; }
-        public ItemStatus Status { get; set; } = ItemStatus.Unstarted;
+        public ItemStatus Status { get; set; } = ItemStatus.Ordered;
         public LineItemStatus LineStatus { get; set; } = LineItemStatus.Locked;
     }
 
-    public enum ItemStatus
+    public enum ItemStatus      // the status of the order item overall
     {
-        Unstarted,
-        InProgress,
-        Done,
-        Served,
-        Returned
+        Ordered,                // initial state
+        AtLine,                 // during entire time it is at preperation line
+        WaitingToBeServed,      // at serving zone
+        Served,                 // served to table
+        Returned                // customer returned item for some reason
     }
 
-    public enum LineItemStatus
+    public enum LineItemStatus  // status of the order item in the line of preperation 
     {
         Locked,
         ToDo,
