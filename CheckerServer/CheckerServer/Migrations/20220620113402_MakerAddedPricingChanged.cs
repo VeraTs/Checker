85738c48c9d03585742276b0531cfdf990a6896c
Vera Ts <veratsvang@gmail.com>
2022-06-20 14:37:04 +0300

    Added to DB migrations updated models, fixed typos

---
+++ CheckerServer/CheckerServer/Migrations/20220620113402_MakerAddedPricingChanged.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckerServer.Migrations
+{
+    public partial class MakerAddedPricingChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPayedFor",
+                table: "OrderItems");
+
+            migrationBuilder.AddColumn<float>(
+                name: "RemainsToPay",
+                table: "Orders",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "TotalCost",
+                table: "Orders",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Dishes",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MakerFit",
+                table: "Dishes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MakerId",
+                table: "Dishes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Maker",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LineId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maker", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Maker_Lines_LineId",
+                        column: x => x.LineId,
+                        principalTable: "Lines",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maker_LineId",
+                table: "Maker",
+                column: "LineId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Maker");
+
+            migrationBuilder.DropColumn(
+                name: "RemainsToPay",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "TotalCost",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "MakerFit",
+                table: "Dishes");
+
+            migrationBuilder.DropColumn(
+                name: "MakerId",
+                table: "Dishes");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPayedFor",
+                table: "OrderItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Dishes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
--- CheckerServer/CheckerServer/Models/Dish.cs
+++ CheckerServer/CheckerServer/Models/Dish.cs
@@ -10,10 +10,10 @@ namespace CheckerServer.Models
         [ForeignKey("Line")]
         public int LineId { get; set; }
         [ForeignKey("RestMenu")]
-        public float RestMenuId { get; set; }
+        public int RestMenuId { get; set; }
         [Required]
-        public int Price { get; set; }
-        [Required]
+        public float Price { get; set; }
+        [ForeignKey("Maker")]
         public int MakerId { get; set; }    // what maker is mainly used to create this dish (stovetop/ oven/ etc.)
         public int MakerFit { get; set; } = -1; // how many of this dish can be made at the same time inside the maker
 
