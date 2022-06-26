65e6ba6fdf8c6f97751a0d2978368d3ee121e740
Vera Ts <veratsvang@gmail.com>
2022-06-26 15:07:17 +0300

    Added Makers api

---
+++ CheckerServer/CheckerServer/Controllers/MakerController.cs
@@ -0,0 +1,24 @@
+﻿using CheckerServer.Data;
+using CheckerServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheckerServer.Controllers
+{
+    [Route("Makers")]
+    [ApiController]
+    public class MakerController : BasicDbController<Maker>
+    {
+        public MakerController(CheckerDBContext context, DbSet<Maker> set) : base(context, set)
+        {
+        }
+
+        protected override void updateItem(Maker existingItem, Maker updatedItem)
+        {
+            if (!string.IsNullOrEmpty(updatedItem.Name))
+            {
+                existingItem.Name = updatedItem.Name;
+            }
+        }
+    }
+}
