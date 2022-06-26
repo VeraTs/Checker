8251efdce667edaa3f3483e885d435d045e7c00c
Aviv Barkai <avivbarkai3@gmail.com>
2022-06-26 14:33:32 +0300

    Update Dish.cs

--- CheckerServer/CheckerServer/Models/Dish.cs
+++ CheckerServer/CheckerServer/Models/Dish.cs
@@ -13,8 +13,8 @@ namespace CheckerServer.Models
         public int RestMenuId { get; set; }
         [Required]
         public float Price { get; set; }
-        [ForeignKey("Maker")]
-        public int MakerId { get; set; }    // what maker is mainly used to create this dish (stovetop/ oven/ etc.)
+
+        [ForeignKey("Maker")] public int MakerId { get; set; } = -1;   // what maker is mainly used to create this dish (stovetop/ oven/ etc.)
         public int MakerFit { get; set; } = -1; // how many of this dish can be made at the same time inside the maker
         public float EstMakeTime { get; set; } = -1; // how much time in minutes it takes to make - an estimate
 
