18d85bb638259a051fd5e753241f64dbf2eafa78
Vera Ts <veratsvang@gmail.com>
2022-07-13 22:11:42 +0300

    Altered and added models and utils for kitchen activity

--- CheckerServer/CheckerServer/Models/LineDTO.cs
+++ CheckerServer/CheckerServer/Models/LineDTO.cs
@@ -3,6 +3,7 @@
     public class LineDTO
     {
         public Line line { get; set; }
+        public int lineId { get; set; }
         public virtual List<OrderItem> LockedItems { get; set; } = new List<OrderItem>();
         public virtual List<OrderItem> ToDoItems { get; set; } = new List<OrderItem>();
         public virtual List<OrderItem> DoingItems { get; set; } = new List<OrderItem>();
