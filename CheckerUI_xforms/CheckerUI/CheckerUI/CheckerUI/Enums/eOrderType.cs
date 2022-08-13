82a51b99d383d5c2983b6bff07e36a8e551d93ea
Aviv Barkai <avivbarkai3@gmail.com>
2022-08-13 16:12:50 +0300

    Update eOrderType.cs
    missed some

--- CheckerUI_xforms/CheckerUI/CheckerUI/CheckerUI/Enums/eOrderType.cs
+++ CheckerUI_xforms/CheckerUI/CheckerUI/CheckerUI/Enums/eOrderType.cs
@@ -3,8 +3,8 @@ namespace CheckerUI.Enums
 {
     public enum eOrderType
     {
-        FIFO,
-        AllTogether,
-        Staggered
+        AllTogether, // all ordered items served together
+        Staggered, // starters first, then mains, then desserts
+        FIFO // whatever is ready first is sent first to the table
     }
 }
