f0fdbb37c00978ae6e7dd075405dcd274bba7e9d
Eliran Shimonov <eliransi@mta.ac.il>
2022-09-05 19:37:52 +0300

    Added properties to Dish: ThisMothSales/LastMothSales/AvargeMonthSales Added properties to OrderItem:Star/Finish rebuilded with the powershell and added the imigrations Added logic to the kitchenHub and to the KitchenManager in the ToDone method

---
+++ CheckerServer/CheckerServer/Migrations/20220904152838_myMigrationName.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckerServer.Migrations
+{
+    public partial class myMigrationName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Finish",
+                table: "OrderItems",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Start",
+                table: "OrderItems",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<int>(
+                name: "AvrageMonthSales",
+                table: "Dishes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LastMonthSales",
+                table: "Dishes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ThisMonthSales",
+                table: "Dishes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Finish",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "Start",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "AvrageMonthSales",
+                table: "Dishes");
+
+            migrationBuilder.DropColumn(
+                name: "LastMonthSales",
+                table: "Dishes");
+
+            migrationBuilder.DropColumn(
+                name: "ThisMonthSales",
+                table: "Dishes");
+        }
+    }
+}
